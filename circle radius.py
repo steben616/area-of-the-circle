2eb270ea0ac52f0b98300b156e8b4017870e2ad8
steben616 <77856591+steben616@users.noreply.github.com>
2021-01-23 00:53:31 +0530

    Add files via upload

---
+++ circle radius.py
@@ -0,0 +1,7 @@
+print("Hello")
+PI = 3.14
+radius = float(input(' please enter the radius of a circle '))
+diameter = 2 * radius
+area = 3.14 * radius * radius
+print(" diameter of a circle = %.2f" %diameter)
+print("Area of a circle = %.2f" %area)
